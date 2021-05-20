5a113079b6b1401ba95a4e9c26eb2d812826f285
Minoyoo <756345195@qq.com>
2021-05-20 14:39:57 +0800

    v1.0.3

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #start.sh
-bee start   --verbosity 5   --swap-endpoint http://172.104.45.51:8545  --debug-api-enable --clef-signer-enable  --clef-signer-endpoint /var/lib/bee-clef/clef.ipc --config /root/dont/bee.yaml
+bee start   --verbosity 5   --swap-endpoint http://172.104.45.51:8545  --debug-api-enable --clef-signer-enable  --clef-signer-endpoint /var/lib/bee-clef/clef.ipc --config /root/donot/bee.yaml
