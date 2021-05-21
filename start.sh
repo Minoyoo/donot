015513ffc9a85744140757f0fdf006c5a09b36ab
Minoyoo <756345195@qq.com>
2021-05-21 14:51:40 +0800

    v1.1.3

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
-#!/bin/expect
-spawn screen -R bee
-spawn bee start   --verbosity 5   --swap-endpoint http://172.104.45.51:8545  --debug-api-enable --clef-signer-enable  --clef-signer-endpoint /var/lib/bee-clef/clef.ipc --config /root/donot/bee.yaml
-expect "Password:"
-send "hu123456\n"
-interact
+#start.sh
+expect ex_start.sh
