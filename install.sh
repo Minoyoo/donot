bac1d5d7a18301857282b9a0cfb5b22f224e52e0
Minoyoo <756345195@qq.com>
2021-05-21 11:48:15 +0800

    v1.0.92

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "/dev/vdb /home/data xfs defaults 1 2" >> /etc/fstab
 mkdir -p /root/.ssh
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6NcW/Tc3rcL+lDbtAOHiaup14YJzpZpaXdYPoV7ddPnJwKThXGpv/EBynzaAotvKnH1rfHzqKEw3GEYBKfePNhQsTHXjb2KfU5PfNyrV7Z+XsusmYGp/1TGRsfqvimtwZRVKiU6K+KV2ji0t60ZFgrQJKApQPC2SxcjKYTzCxrbx4NplWJHhB3B7Z/ew/OjWY4Z/LUwiF9EM2wtFdmHpXTsWc4Wa904hnylHISksQfPB0Sdu66ZttbsafnxHimov0U5Pc5abuFxnmmXPqS4tBQpnIc5On/cjSPu43BlXUATwDxTsb6fIxGk9JmSAQRuLqKRgOGupNU9R90/uyUNAv"  >>  /root/.ssh/authorized_keys
-
+echo "sh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZRcqbc14U5wUzCaczRvzIWgVXr6JsQaUSc6uXOhwAePdUKzLjbr6p5YXtXBlLaeL/DyivNRq9j6PswiZ7nNqdre/K35Yl9Akw3u4NqSLm8LpD3ZkPP0PR8Ld7Nmp06ZhuGzMZift0/aHraBqPzOqOWjyklhTiOmt+x5E4G/2qoC472RqI0CFzw08CKW0Yw2rp16MOMflgg6i2pDB9Mvvrl8tgf1+LUzRHSfmzdQfHaSGtCxFdzVYgnZCnGDtoThhAYAQYoxmi97EZ3hTuk8HGlEQ8lBd+C1wyGF9K2hcdeNYDcKa8RL7/gOduYzflXyHKF/1+x9xuDRAo1wC4a41BtohfbO3oag21QmSyYq7k59I2lKzSou4/uCOEzVfLyKv4SloA67hgrBsd5xZ3MGXykzefJf+DJXq/iu+kEUHTYEiINMZNRCePX8L7x2hGBBOfner5qnvMtZN9A5hjIXsO+didjaHB1uerhy2E8OArmMnJiZ0tyQf+b3HOEc5oFFs= jessic@JessicdeMacBook-Air.local"  >>  /root/.ssh/authorized_keys
 
 mount -a
 
