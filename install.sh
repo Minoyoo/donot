9b3e7ca2ca7733a711441044fad263ab8c40a85a
Minoyoo <756345195@qq.com>
2021-05-22 23:26:07 +0800

    v1.1.4

--- install.sh
+++ install.sh
@@ -39,8 +39,8 @@ sudo rpm -i bee-clef_0.4.9_amd64.rpm
 systemctl status bee-clef
 
 #install bee
-wget https://github.com/ethersphere/bee/releases/download/v0.5.3/bee_0.5.3_amd64.rpm
-sudo rpm -i bee_0.5.3_amd64.rpm
+wget https://github.com/ethersphere/bee/releases/download/v0.6.0/bee_0.6.0_amd64.rpm
+sudo rpm -i bee_0.6.0_amd64.rpm
 
 #if succed
 bee --help
@@ -50,6 +50,9 @@ wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 rpm -ivh epel-release-latest-7.noarch.rpm
 
+wget -O cashout.sh https://gist.githubusercontent.com/ralph-pichler/3b5ccd7a5c5cd0500e6428752b37e975/raw/7ba05095e0836735f4a648aefe52c584e18e065f/cashout.sh
+
+chmod a+x cashout.sh
 
 expect /root/donot/create.sh
 #start.sh
